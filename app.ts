aea66364665d8277e68a63c3542d95c62f3293fa
IgorRamosg13 <igor_ramos13@hotmail.com>
2018-03-01 00:06:23 -0300

    Criando a classe 'Carro' dentro de app.ts e app.js (a partir do typescript). Adicionado já métodos e atributos.

---
+++ app.ts
@@ -0,0 +1,32 @@
+class Carro{
+    private modelo: string
+    private numeroDePortas: number
+    private velocidade: number=0
+
+    constructor(modelo: string, numeroDePortas: number) {
+        this.modelo = modelo
+        this.numeroDePortas = numeroDePortas
+    }
+
+    public acelerar(): void{
+        this.velocidade = this.velocidade +10
+    }
+
+    public parar(): void{
+        this.velocidade = 0
+    }
+
+    public velocidadeAtual(): number{
+        return this.velocidade
+    }
+} 
+
+//Instanciando um Carro passando o modelo como parâmetro
+let carroA = new Carro('Veloster', 3)
+
+//Apresentando objeto instanciado
+console.log(carroA)
+
+//Chamando Método
+carroA.acelerar()
+console.log(carroA)
--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,25 @@
 "use strict";
+var Carro = /** @class */ (function () {
+    function Carro(modelo, numeroDePortas) {
+        this.velocidade = 0;
+        this.modelo = modelo;
+        this.numeroDePortas = numeroDePortas;
+    }
+    Carro.prototype.acelerar = function () {
+        this.velocidade = this.velocidade + 10;
+    };
+    Carro.prototype.parar = function () {
+        this.velocidade = 0;
+    };
+    Carro.prototype.velocidadeAtual = function () {
+        return this.velocidade;
+    };
+    return Carro;
+}());
+//Instanciando um Carro passando o modelo como parâmetro
+var carroA = new Carro('Veloster', 3);
+//Apresentando objeto instanciado
+console.log(carroA);
+//Chamando Método
+carroA.acelerar();
+console.log(carroA);
